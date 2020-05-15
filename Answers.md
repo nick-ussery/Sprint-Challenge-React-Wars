0cc8c6674b8363db8433e4e0e3a998aa92f4ebe0
nick-ussery <nick.ussery@gmail.com>
2020-05-15 11:39:58 -0500

    added axios

--- Answers.md
+++ Answers.md
@@ -2,8 +2,16 @@
 
 1. What is React JS and what problems does it solve? Support your answer with concepts introduced in class and from your personal research on the web.
 
+a virtual dom manipulator, makes a webpage dynamic giving faster load time
+
 1. Describe component state.
+
+component state is the ability to remember and hold a value after the page has rendered
 
 1. Describe props.
 
+props are the parameters passed down to a component from a parent
+
 1. What are side effects, and how do you sync effects in a React component to changes of certain state or props?
+
+side effects are the other effects of an action that are unintended. useEffect can point to an array, takes an array as its second parameter
--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import {Container, Row} from 'reactstrap'
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 150px;
+  height: auto;
+  radius: 5%;
+  background-color: black;
+  color: white;
+`
+
+const CharacterRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 25px;
+  text-align: center;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,11 +29,51 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const [characterList, setCharacterList] = useState("");
+const [nextPage, setNextPage] = useState("");
+const [previousPage, setPreviousPage] = useState('');
+const [currentPage, setPage] = useState('https://rickandmortyapi.com/api/character/?page=1')
 
+const pageHandler = (direction)=>{
+  if(direction==='next'){setPage(nextPage)
+  }else if(direction==='prev'){
+    if(!previousPage){
+
+    }else{
+      setPage(previousPage);
+    }
+  }
+}
+
+let allCharacters =[];
+useEffect(()=>{
+  axios.get(currentPage)
+  .then(response=>{
+    console.log(response)
+    setCharacterList(response.data.results)
+    setNextPage(response.data.info.next)
+    setPreviousPage(response.data.info.prev)
+  })
+  .catch(err=>{console.log("Error", err)})
+
+}, [currentPage])
+
+if(characterList.length !==0){
+  allCharacters =characterList.map((character)=>{
+    return <Character key={character.id} name={character.name} episodes={character.episode.length} status={character.status} species={character.species}gender={character.gender}origin={character.origin} location={character.location} image={character.image}/>
+  })
+}
+
+// console.log("characterList", characterList);
   return (
-    <div className="App">
+    <Container className="App">
       <h1 className="Header">Characters</h1>
-    </div>
+      <CharacterRow>
+      {allCharacters}
+      </CharacterRow>
+      <Button onClick={()=>{pageHandler('prev')}}>Previous</Button>
+      <Button onClick={()=>{pageHandler('next')}}>Next</Button>
+    </Container>
   );
 }
 
